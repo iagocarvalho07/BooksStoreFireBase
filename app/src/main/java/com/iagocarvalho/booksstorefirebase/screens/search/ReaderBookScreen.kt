90b7f38338f1ab3e32616ccba041657164c9e56c
iagocarvalho07 <games.carvalho@hotmail.com>
2023-07-19 10:03:31 -0300

    criando tela de livros api

--- app/src/main/java/com/iagocarvalho/booksstorefirebase/screens/search/ReaderBookScreen.kt
+++ app/src/main/java/com/iagocarvalho/booksstorefirebase/screens/search/ReaderBookScreen.kt
@@ -1,28 +1,45 @@
 package com.iagocarvalho.booksstorefirebase.screens.search
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.iagocarvalho.booksstorefirebase.components.InputField
 import com.iagocarvalho.booksstorefirebase.components.ReaderAppBar
+import com.iagocarvalho.booksstorefirebase.model.MBook
 import com.iagocarvalho.booksstorefirebase.navigation.ReaderScreens
 
 @Preview
@@ -41,16 +58,72 @@ fun ReaderBookSearchScreen(navController: NavController = NavController(LocalCon
                 navController.navigate(ReaderScreens.ReaderHomeScreen.name)
             }
         }) { contentPadding ->
-        Box(modifier = Modifier.padding(contentPadding)){
+        Box(modifier = Modifier.padding(contentPadding)) {
             Surface() {
                 Column {
-                    SearchForm(modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp))
+                    SearchForm(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    )
+                    Spacer(modifier = Modifier.height(13.dp))
+                    BookList(navController)
 
                 }
-            }
 
+            }
+        }
+    }
+}
+
+@Composable
+fun BookList(navController: NavController) {
+    val listOfBooks = listOf(
+        MBook("asad", "hello ", "all of us ", "algusm"),
+        MBook("asad", "hasdello ", "all of us ", "algusm"),
+        MBook("asad", "hasdasello ", "all of us ", "algusm"),
+        MBook("asad", "heddllo ", "all of us ", "algusm"),
+        MBook("asad", "asda ", "asdasd of us ", "algusm"),
+    )
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        items(items = listOfBooks) { book ->
+            BookRow(book, navController)
+        }
+        //items(items = listOfBooks) { book -> BookRow(book, navController) }
+
+
+    }
+}
+
+@Composable
+fun BookRow(book: MBook, navController: NavController) {
+    Card(
+        modifier = Modifier
+            .clickable { }
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(3.dp),
+        shape = RectangleShape,
+        elevation = CardDefaults.cardElevation(7.dp),
+    ) {
+        Row(
+            modifier = Modifier.padding(5.dp),
+            verticalAlignment = Alignment.Top
+        ) {
+            val imageUrl = ""
+            AsyncImage(model = imageUrl, contentDescription = "")
+            Column() {
+                Text(text = book.title, overflow = TextOverflow.Ellipsis)
+                Text(
+                    text = "Author  ${book.authors}",
+                    overflow = TextOverflow.Clip,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                // add more later
+            }
         }
     }
 }
