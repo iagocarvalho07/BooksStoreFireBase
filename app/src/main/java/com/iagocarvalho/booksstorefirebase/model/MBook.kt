d72c8993ee49c29205fa952eba61ee5b227bdcc3
iagocarvalho07 <games.carvalho@hotmail.com>
2023-07-24 10:58:58 -0300

    get e set para fireStoreDataBase

--- app/src/main/java/com/iagocarvalho/booksstorefirebase/model/MBook.kt
+++ app/src/main/java/com/iagocarvalho/booksstorefirebase/model/MBook.kt
@@ -1,19 +1,62 @@
 package com.iagocarvalho.booksstorefirebase.model
 
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.PropertyName
+import java.sql.Timestamp
+
 data class MBook (
-   val id: String,
-   val title: String,
-  val  authors: String,
-   val notes: String,
 
+    @Exclude var id: String? = null,
 
-) {
-    fun toMap(): MutableMap<String, Any>{
-        return mutableMapOf(
-            "book-id" to this.id,
-            "display_Tittle" to this.title,
-            "autor" to this.authors,
-            "notes" to this.notes,
-        )
-    }
-}
+    @get:PropertyName("title")
+    @set:PropertyName("title")
+    var title: String? = null,
+
+    @get:PropertyName("authors")
+    @set:PropertyName("authors")
+    var  authors: String? = null,
+
+    @get:PropertyName("notes")
+    @set:PropertyName("notes")
+    var notes: String? = null,
+
+    @get:PropertyName("photo_url")
+    @set:PropertyName("photo_url")
+    var photoURl: String? = null,
+
+    @get:PropertyName("category")
+    @set:PropertyName("category")
+    var category: String? = null,
+
+    @get:PropertyName("publishe_data")
+    @set:PropertyName("publishe_data")
+    var publisheData: String? = null,
+
+    @get:PropertyName("reating")
+    @set:PropertyName("reating")
+    var rating: Double? = null,
+
+    @get:PropertyName("descripiton")
+    @set:PropertyName("descripiton")
+    var description: String? = null,
+
+    @get:PropertyName("page_count")
+    @set:PropertyName("page_count")
+    var pageCount: String? = null,
+
+    @get:PropertyName("started_raiding")
+    @set:PropertyName("started_raiding")
+    var startedRaiding: Timestamp? = null,
+
+    @get:PropertyName("fineshe_reading")
+    @set:PropertyName("fineshe_reading")
+    var finisheReading: Timestamp? = null,
+
+    @get:PropertyName("user_id")
+    @set:PropertyName("user_id")
+    var userId: String? = null,
+
+    @get:PropertyName("google_book_id")
+    @set:PropertyName("google_book_id")
+    var googleBookId: String? = null,
+)
--- app/src/main/java/com/iagocarvalho/booksstorefirebase/screens/details/ReaderBooksDetailsScreen.kt
+++ app/src/main/java/com/iagocarvalho/booksstorefirebase/screens/details/ReaderBooksDetailsScreen.kt
@@ -1,14 +1,24 @@
 package com.iagocarvalho.booksstorefirebase.screens.details
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,20 +26,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.core.text.HtmlCompat
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.google.firebase.firestore.FirebaseFirestore
 import com.iagocarvalho.booksstorefirebase.components.ReaderAppBar
 import com.iagocarvalho.booksstorefirebase.data.Resource
+import com.iagocarvalho.booksstorefirebase.model.MBook
 import com.iagocarvalho.booksstorefirebase.model.ModelApi.Item
 import com.iagocarvalho.booksstorefirebase.navigation.ReaderScreens
+import com.iagocarvalho.booksstorefirebase.screens.home.RounderButton
+import com.iagocarvalho.booksstorefirebase.screens.search.BookSearchViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BookDetailsScreen(
     navController: NavController,
     bookId: String?,
-    viewModel: ReaderBooksDetailsViewModel = hiltViewModel()
+    viewModel: ReaderBooksDetailsViewModel = hiltViewModel(),
 ) {
 
     Scaffold(topBar = {
@@ -52,11 +73,11 @@ fun BookDetailsScreen(
                     verticalArrangement = Arrangement.Top,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    val bookinfo = produceState<Resource<Item>>(initialValue = Resource.Loading()){
+                    val bookinfo = produceState<Resource<Item>>(initialValue = Resource.Loading()) {
                         value = viewModel.getBookInfo(BookId = bookId!!)
                     }.value
 
-                    if (bookinfo.data == null){
+                    if (bookinfo.data == null) {
                         Column(
                             modifier = Modifier.padding(16.dp),
                             horizontalAlignment = Alignment.CenterHorizontally,
@@ -64,11 +85,11 @@ fun BookDetailsScreen(
                         ) {
                             LinearProgressIndicator()
                             Text(text = "Loading....")
-                            
+
                         }
-                    }else{
-                        Text(text = "Book  ${bookinfo.data.volumeInfo.title}")
-                        Text(text = bookId!!)
+                    } else {
+                        ShowBooksDetails(bookinfo, navController)
+
                     }
 
                 }
@@ -80,4 +101,79 @@ fun BookDetailsScreen(
 
     }
 
+}
+
+@Composable
+fun ShowBooksDetails(bookinfo: Resource<Item>, navController: NavController) {
+
+    val bookData = bookinfo.data!!.volumeInfo
+    val googleBookId = bookinfo.data.id
+
+
+
+    Card(
+        modifier = Modifier.padding(34.dp),
+        shape = CircleShape,
+        elevation = CardDefaults.cardElevation(34.dp)
+    ) {
+        AsyncImage(
+            model = bookData!!.imageLinks.thumbnail,
+            contentDescription = "book image",
+            modifier = Modifier
+                .width(90.dp)
+                .height(90.dp)
+                .padding(1.dp)
+        )
+
+    }
+    Text(
+        text = bookData.title,
+        style = MaterialTheme.typography.titleLarge,
+        overflow = TextOverflow.Ellipsis, maxLines = 19
+    )
+    Text(text = "Autores : ${bookData.authors}")
+    Text(text = "Page Count : ${bookData.pageCount}")
+    Text(text = "Categories : ${bookData.categories}", style = MaterialTheme.typography.bodySmall)
+    Text(text = "publisher : ${bookData.publisher}")
+    Spacer(modifier = Modifier.height(5.dp))
+
+    val cleanDescription = HtmlCompat.fromHtml(
+        bookData.description,
+        HtmlCompat.FROM_HTML_MODE_LEGACY
+    ).toString()
+    val localDims = LocalContext.current.resources.displayMetrics
+
+    Surface(
+        modifier = Modifier
+            .height(localDims.heightPixels.dp.times(0.09f))
+            .padding(4.dp),
+        shape = RectangleShape,
+        border = BorderStroke(1.dp, Color.DarkGray)
+    ) {
+        LazyColumn(modifier = Modifier.padding(3.dp)) {
+            item {
+                Text(text = cleanDescription)
+            }
+        }
+
+    }
+    Row(modifier = Modifier.padding(top = 6.dp), horizontalArrangement = Arrangement.SpaceAround) {
+        RounderButton(label = "Save"){
+            // save this book to the firestore
+            val book = MBook()
+
+            saveToFireBase(book)
+        }
+        Spacer(modifier = Modifier.width(25.dp))
+        RounderButton(label = "Cancel"){
+            navController.popBackStack()
+        }
+    }
+
+
+}
+
+fun saveToFireBase(book: MBook) {
+    val db = FirebaseFirestore.getInstance()
+    TODO("Not yet implemented")
 }
