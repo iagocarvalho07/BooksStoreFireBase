412e59dd65b17ea31107bf10c080460202084fb9
iagocarvalho07 <games.carvalho@hotmail.com>
2023-07-26 15:06:11 -0300

    creat a update firestore

--- app/src/main/java/com/iagocarvalho/booksstorefirebase/components/Components.kt
+++ app/src/main/java/com/iagocarvalho/booksstorefirebase/components/Components.kt
@@ -1,9 +1,16 @@
 package com.iagocarvalho.booksstorefirebase.components
 
+import android.view.MotionEvent
+import android.widget.RatingBar
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,12 +32,19 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -41,6 +55,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
+import com.iagocarvalho.booksstorefirebase.R
 import com.iagocarvalho.booksstorefirebase.navigation.ReaderScreens
 
 @Composable
@@ -207,7 +222,7 @@ fun ReaderAppBar(
                     navController.navigate(ReaderScreens.LoginScreen.name)
                 }
             }) {
-                if (showProfile) Row(){
+                if (showProfile) Row() {
                     Icon(
                         imageVector = Icons.Default.ExitToApp,
                         contentDescription = "Exit App",
@@ -237,5 +252,52 @@ fun FABcontent(onTap: () -> Unit) {
             tint = MaterialTheme.colorScheme.primary
         )
 
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun RatingBar(
+    modifier: Modifier = Modifier,
+    rating: Int,
+    onPressRating: (Int) -> Unit
+) {
+    var ratingState by remember { mutableStateOf(rating) }
+    var selected by remember { mutableStateOf(false) }
+    val size by animateDpAsState(
+        targetValue = if (selected) 42.dp else 34.dp,
+        spring(Spring.DampingRatioMediumBouncy)
+    )
+    Row(
+        modifier = Modifier.width(280.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        for (i in 1..5) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_baseline_star_24),
+                contentDescription = "Star",
+                modifier = Modifier
+                    .width(size)
+                    .height(size)
+                    .pointerInteropFilter {
+                        when (it.action) {
+                            MotionEvent.ACTION_DOWN -> {
+                                selected = true
+                                onPressRating(i)
+                                ratingState = i
+                            }
+
+                            MotionEvent.ACTION_UP -> {
+                                selected = false
+
+                            }
+                        }
+                        true
+                    },
+                tint = if (i <= ratingState) Color(0xFFFFD700) else Color(0XFFA2ADB1)
+            )
+
+        }
     }
 }
--- app/src/main/java/com/iagocarvalho/booksstorefirebase/screens/update/ReaderBookUpdadeScreen.kt
+++ app/src/main/java/com/iagocarvalho/booksstorefirebase/screens/update/ReaderBookUpdadeScreen.kt
@@ -1,6 +1,7 @@
 package com.iagocarvalho.booksstorefirebase.screens.update
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
@@ -24,21 +26,33 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.FirebaseFirestore
+import com.iagocarvalho.booksstorefirebase.components.InputField
 import com.iagocarvalho.booksstorefirebase.components.ReaderAppBar
 import com.iagocarvalho.booksstorefirebase.data.DataOrException
 import com.iagocarvalho.booksstorefirebase.model.MBook
 import com.iagocarvalho.booksstorefirebase.screens.home.HomeScreenViewModel
+import com.iagocarvalho.booksstorefirebase.screens.home.RounderButton
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -87,12 +101,148 @@ fun BookUpadateScreen(
                             ShowBookUpdate(bookinfo = viewModel.data.value, bookItemId = bookItemId)
 
                         }
+                        ShowSimpleForm(
+                            book = viewModel.data.value.data?.first { mBook -> mBook.googleBookId == bookItemId }!!,
+                            navController
+                        )
                     }
 
+
+                    Spacer(modifier = Modifier.padding(bottom = 15.dp))
+
+                }
+            }
+        }
+
+    }
+
+}
+
+@Composable
+fun ShowSimpleForm(book: MBook, navController: NavController) {
+
+    val notesText = remember { mutableStateOf("") }
+    val isStartedReding = remember { mutableStateOf(false) }
+    val isFinishedReading = remember { mutableStateOf(false) }
+    val ratingVal = remember { mutableStateOf(0) }
+    SimpleForm(
+        defaultValue = if (book.notes.toString()
+                .isNotEmpty()
+        ) book.notes.toString() else ""
+    ) { note ->
+        notesText.value = note
+    }
+    Row(
+        modifier = Modifier.padding(4.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        TextButton(
+            onClick = { isStartedReding.value = true },
+            enabled = book.startedRaiding == null
+        ) {
+            if (book.startedRaiding == null) {
+                if (!isStartedReding.value) {
+                    Text(text = "Start Reading")
+                } else {
+                    Text(
+                        text = "Started Reading",
+                        modifier = Modifier.alpha(0.6f),
+                        color = Color.Red.copy(alpha = 0.5f)
+                    )
+                }
+            } else {
+                Text(text = "Started On ${book.startedRaiding}")
+
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+            TextButton(
+                onClick = { isFinishedReading.value = true },
+                enabled = book.finisheReading == null
+            ) {
+                if (book.finisheReading == null) {
+                    if (!isFinishedReading.value) {
+                        Text(text = "Mark as read")
+                    } else {
+                        Text(text = "Finished Reading!")
+                    }
+                } else {
+                    Text(text = "Finished On ${book.finisheReading}")
                 }
 
             }
         }
+    }
+    Text(text = "Rating", modifier = Modifier.padding(bottom = 3.dp))
+//        book.rating?.toInt().let {
+//           RatingBar(rating = it!! ){rating ->
+//               ratingVal.value = rating
+//           }
+//        }
+    Row(horizontalArrangement = Arrangement.SpaceAround) {
+
+        val chagendNote = book.notes != notesText.value
+        val chagedRating = book.rating?.toInt() != ratingVal.value
+        val isFinishedTimeStamp =
+            if (isFinishedReading.value) Timestamp.now() else book.finisheReading
+        val isStaredTimestamp =
+            if (isStartedReding.value) Timestamp.now() else book.startedRaiding
+
+        val bookToUpadate = chagendNote || chagedRating || isStartedReding.value || isFinishedReading.value
+        val bookupdate = hashMapOf(
+            "fineshe_reading" to isFinishedTimeStamp,
+            "started_raiding" to isStaredTimestamp,
+            "reating" to ratingVal,
+            "notes" to notesText.value
+        ).toMap()
+
+        RounderButton(label = "Upadate") {
+            if (bookToUpadate){
+                FirebaseFirestore.getInstance()
+                    .collection("books")
+                    .document(book.id!!)
+                    .update(bookupdate)
+                    .addOnCompleteListener{ task->
+                        Log.d("TAG", "ShowSimpleForm:  ${task.result.toString()}")  }
+                    .addOnFailureListener { task -> Log.w("ERROR", "ERROR TO UPDATE DOCUMENT : ${task.message}") }
+            }
+
+
+        }
+        RounderButton(label = "Delete") {}
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun SimpleForm(
+    modifier: Modifier = Modifier,
+    loading: Boolean = false,
+    defaultValue: String = "Great Book!",
+    onSerch: (String) -> Unit
+) {
+    Column() {
+        val TexFildValue = rememberSaveable { mutableStateOf(defaultValue) }
+        val keyboardController = LocalSoftwareKeyboardController.current
+        val valid = remember(TexFildValue) {
+            TexFildValue.value.trim().isNotEmpty()
+
+        }
+
+        InputField(
+            modifier = Modifier
+                .height(140.dp)
+                .padding(3.dp)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp),
+            valueState = TexFildValue,
+            labelId = "Entrer",
+            enabled = true,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onSerch(TexFildValue.value.trim())
+                keyboardController?.hide()
+            })
 
     }
 
